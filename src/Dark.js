6a90b331a8f8a28769b3beaa5e0c1208fbbfe7db
Yx_Zou <hello.yxhome@gmail.com>
2020-05-29 15:24:38 +0800

    完成基本配色方案，还剩评论与页面其它属性的调整

--- src/Dark.js
+++ src/Dark.js
@@ -6,33 +6,44 @@
 // @grant        none
 // ==/UserScript==
 
-// background-image: url(https://gitee.com/Yx_z/figurebed/raw/master/vscode/VsCode.jpg);
-
 
 // 配置项
-
+// https://gitee.com/Yx_z/figurebed/raw/master/vscode/vscode-bg.png
 var config = {
-    //背景（图床地址）
-    bg: 'https://gitee.com/Yx_z/figurebed/raw/master/vscode/vscode-bg.png',
     // 延时时间（单位/秒）
-    timeDelay: 2
+    timeDelay: 2,
+
+    //背景（图床地址）
+    bg: '',
+    //背景颜色
+    bgColor: '#121212',
+
+    // 内容字体颜色
+    contentColor: '#dcdcdc',
+    // 加粗字体颜色
+    boldColor: '#ffffff',
+    // 小标题颜色
+    headColor: '#ffffff',
+
+    // 文中音频背景色
+    contentVideoColor: '#1d1d1c',
+    // 文中音频边框色
+    contentVideoBorder: '1px solid #2e2d2d',
+
+    // 代码块字体色
+    codeColor: '#7f7f7f',
+    // 代码块背景色
+    codeBgColor: '#1f2021',
+    // 代码块边框属性
+    codeBorder: '1px solid #2b2c2f'
+
     // 必须
-    // 文中主要内容字体颜色
-    // 文中小标题颜色
-    // 文中代码块背景色与边框色
-    // 文中加粗字体颜色
     // 非必须
-    // 左右按钮切换背景色
-    // 播放器背景色与边框色
-    // 小播放器背景色与边框色
-    // 退出沉浸模式阅读背景色
+    // 左右按钮切换背景色 #f6f7f9 color #999 border 1px solid #f6f7f9
+    // 小播放器背景色与边框色 边框 1px solid #f0f0f0
+    // 退出沉浸模式阅读背景色   #f6f7f9
+    // 作者回复 字体 #505050 bg #f6f7fb
 }
-// 背景图片（图床地址）
-var bg = 'https://gitee.com/Yx_z/figurebed/raw/master/vscode/vscode-bg.png'
-
-// 设置延时时间（单位/秒）
-var timeDelay = 2
-
 
 // immerseDom 沉浸点击 dom
 // bgImageDom 背景图片 dom
@@ -56,7 +67,7 @@ window.onload = function () {
         // 内容节点
         contentDom = bgImageDom.children[0].children[0].children[0].children[0].children[2]
 
-    }, timeDelay * 1000)
+    }, config.timeDelay * 1000)
 }
 
 function immerseClick() {
@@ -69,22 +80,23 @@ function immerseClick() {
     hideImages()
     // 设置音频属性
     setVideoStyle()
-    // 找出多个代码块文本节点
-    getPre()
+    // 设置文中普通字体颜色
+    setContentColor()
+    // 内容属性设置
+    contentStyle()
 }
 
 // 设置背景图片
 function setBgImage(image) {
-    let imageUrl
-    if (!image) {
-        imageUrl = bg
+    if (config.bg) {
+        bgImageDom.style.backgroundImage = "url(" + config.bg + ")";//设置背景图的的地址
+        bgImageDom.style.backgroundRepeat = "no-repeat";//设置背景不平铺
+        bgImageDom.style.backgroundPosition = "center";//设置背景图的位置
+        bgImageDom.style.backgroundSize = "auto";//设置背景图像的尺寸
     } else {
-        imageUrl = image
+        bgImageDom.style.backgroundColor = config.bgColor
     }
-    bgImageDom.style.backgroundImage = "url(" + imageUrl + ")";//设置背景图的的地址
-    bgImageDom.style.backgroundRepeat = "no-repeat";//设置背景不平铺
-    bgImageDom.style.backgroundPosition = "center";//设置背景图的位置
-    bgImageDom.style.backgroundSize = "auto";//设置背景图像的尺寸
+
 }
 
 // 隐藏设置背景图后的出现的白边
@@ -95,8 +107,9 @@ function hideWhiteBorder() {
     leftBorderDom.style.width = '379px'
 }
 
-// 隐藏文中首页与末尾图片
+// 首尾图片隐藏
 function hideImages() {
+    // 隐藏文中首页与末尾图片
     contentDom.children[0].style.display = "none"
     contentDom.children[contentDom.children.length - 2].style.display = "none"
 }
@@ -104,14 +117,38 @@ function hideImages() {
 // 设置音频属性
 function setVideoStyle() {
     let videoDom = contentDom.children[1]
-    videoDom.style.backgroundColor = '#282c34'
-    videoDom.style.border = '1px solid #282c34'
+    videoDom.style.backgroundColor = config.contentVideoColor
+    videoDom.style.border = config.contentVideoBorder
 }
 
-// 找出多个代码块文本节点
-function getPre() {
+// 设置文字字体颜色
+function setContentColor() {
+    // 文本内字体节点
+    let fontDom = contentDom.children[2].children[0].children[0].children[0]
+    fontDom.style.color = config.contentColor
+}
+
+// 内容属性设置
+function contentStyle() {
+    // 获取代码块节点
     let preList = contentDom.querySelectorAll('[data-slate-type="pre"]')
+    // 获取加粗字体节点
+    let boldList = contentDom.querySelectorAll('[data-slate-type="bold"]')
+    // 获取文中小标题
+    let headingList = contentDom.querySelectorAll('[data-slate-type="heading"]')
+
+    // 遍历设置属性
     preList.forEach(item => {
-        item.style.backgroundColor = '#282c34'
+        item.style.backgroundColor = config.codeBgColor
+        item.style.color = config.codeColor
+        item.style.border = config.codeBorder
+    });
+
+    boldList.forEach(item => {
+        item.style.color = config.boldColor
+    });
+
+    headingList.forEach(item => {
+        item.style.color = config.headColor
     });
 }
